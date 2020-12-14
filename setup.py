06ca52056d3371194a217b225e83fd0b2e35986a
Adel KARA SLIMANE <adel@karaslimane.com>
2020-12-14 19:23:25 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='csv_manager',
-      version='0.10.4',
+      version='0.10.5',
       description='A simple CSV importer/exporter with plotting capabilities with matplotlib',
       long_description=readme(),
       long_description_content_type="text/markdown",
